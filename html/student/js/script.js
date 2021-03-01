cfe3a8b986bed4e2335c824ec50b32607852ead0
arec-colon-aldeguer <70238542+arec-colon-aldeguer@users.noreply.github.com>
2021-03-01 13:02:33 -0800

    Delete html/student/js directory

--- html/student/js/script.js
+++
@@ -1,7 +0,0 @@
-let btnShow = document.querySelector('button');
-let input = document.querySelector('input');
-
-input.addEventListener('keyup', () => {
-  if (input.value.length > 0) btnShow.disabled = false
-  else btnShow.disabled = true;
-});
