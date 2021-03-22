11f36f946e80195ba8c9dd0eed7c385cb2f566e6
Sam Mazarei <sinux1@programmer.net>
2021-03-22 20:02:28 +0000

    Added/refactored retrieveMessage, deleteMessage, and retrieveObject

--- src/backend/BackEndMeth.py
+++ src/backend/BackEndMeth.py
@@ -1,13 +1,15 @@
 import json
 import boto3
 
-sqs_client = boto3.client('sqs')
-QueueUrl = "https://sqs.us-east-1.amazonaws.com/208939558331/submitter_sqs"
-bucket_name = 'comp350-submitter-bucket'
-s3_client = boto3.client('s3')
 
-def retrieveMessageFromQueue(sqsClient, queURL, bucketName):
-    pass
+#sqs_client = boto3.client('sqs')
+#QueueUrl = "https://sqs.us-east-1.amazonaws.com/208939558331/submitter_sqs"
+#bucket_name = 'comp350-submitter-bucket'
+#s3_client = boto3.client('s3')
+
+
+
+def retrieveMessageFromQueue(sqsClient, queURL):
     
     sqs_response = sqs_client.receive_message(
         QueueUrl=QueueUrl,
@@ -18,30 +20,27 @@ def retrieveMessageFromQueue(sqsClient, queURL, bucketName):
         MessageAttributeNames=[
             'All'
         ],
-        VisibilityTimeout=15,
+        VisibilityTimeout=150,
         WaitTimeSeconds=0
     )
 
-message = sqs_response['Messages'][0] #get message from sqs queue
-filename = message['filename'] #get filename from message
-bucket_name = 'comp350-submitter-bucket' #bucket name
-def retrieveObjectFromBucket():
-    s3_response = s3_client.get_object(Bucket=bucket_name, Key=filename)
+    message = sqs_response['Messages'][0] #get message from sqs queue
+    return message
+
+
+def retrieveObjectFromBucket(s3bucket='comp350-submitter-bucket' , filename):
+    s3_response = s3_client.get_object(Bucket=s3bucket, Key=filename)
     return s3_response
 
-#use s3_response to process object?
 
 
 
+def deleteMessageFromQueue(sqsClient, queURL, message):
+    #delete message
+    receipt_handle = message['ReceiptHandle']
+    sqs_client.delete_message(
+        QueueUrl=QueueUrl,
+        ReceiptHandle=receipt_handle
+    )
 
-
-#delete message
-receipt_handle = message['ReceiptHandle']
-sqs_client.delete_message(
-    QueueUrl=QueueUrl,
-    ReceiptHandle=receipt_handle
-)
-
-#sqs queue name: submitter_sqs
-#sqs queue url: https://sqs.us-east-1.amazonaws.com/208939558331/submitter_sqs
 
