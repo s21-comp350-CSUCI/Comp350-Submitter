289ca4751703207a7601e827bc9d23d1857f5fb3
sinux <sinux1@programmer.net>
2021-04-06 14:26:51 -0700

    Put executable scripts into an executable directory, added the skeleton of the processStudentTestCases.py ademon.

---
+++ src/backend/executable/processStudentTestCases.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+# server.py - a simple threaded server
+import threading
+
+
+
+class ClientThread(threading.Thread):
+  def __init__(self, subID):
+    threading.Thread.__init__(self)
+    self.subID = subID
+
+  def run(self):
+    # This is the entry point for the thread. At this point in the code
+    # the constructor has executed
+    pass
+
+  pass
+  # All the functionality described in backendmeth.md
+  # Will be defined and executed in this thread class
+
+#########################################################################
+#########################################################################
+  
+def main():
+  
+  
+  
+  while True:
+    # Poll for a message on the queue
+    newMessage = pollSQS()
+    # retrieve the subID from the json formated newMessage
+    subID = extractSubID(newMessage)
+    # pass the subID off to a worker thread and move on
+    t = ClientThread(subID)
+    t.start()
+    continue
+
+  
+if __name__ == "__main__":
+    main()
